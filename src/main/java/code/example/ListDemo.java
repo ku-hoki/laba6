179f48e180fd1998da5aca775eef5bce50b2880d
ku-hoki <kutts858@gmail.com>
2024-02-12 16:41:11 +0600

    2 commit

--- src/main/java/code/example/ListDemo.java
+++ src/main/java/code/example/ListDemo.java
@@ -4,12 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListDemo extends Human{
-    public List<Human> findPeopleWithSameSurname(List<Human>humanList, Human person){
+    public ListDemo(String surname, String name, String middleName, int age) {
+        super(surname, name, middleName, age);
+    }
+
+    public List<Human> findPeopleWithSameSurname(List<Human> humanList, Human person){
         List<Human> result = new ArrayList<>();
         String surName = person.getSurname();
         for (Human human: humanList){
-            if (human.getSurname().equals(surName))
-
+            if (human.getSurname().equals(surName) && !human.equals(person)){
+                result.add(human);
+            }
         }
+        return result;
     }
 }
