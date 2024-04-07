1e06566eb5d94181cbf8353fc607f4b631196535
kutts <kutts858@gmail.com>
2024-04-07 06:59:21 +0600

    1-7 task

--- src/main/java/code/example/ListDemo.java
+++ src/main/java/code/example/ListDemo.java
@@ -1,23 +1,53 @@
 package code.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class ListDemo extends Human{
-    public ListDemo(String surname, String name, String middleName, int age) {
-        super(surname, name, middleName, age);
+public class ListDemo {
+
+    //Метод для поиска челиков с одной фамилией
+    public List<Human> findPeopleWithSameSurname(List<Human> humanList, Human person) {
+        List<Human> result = new ArrayList<>();
+        for (Human human : humanList) {
+            if (human.getSurname().equals(person.getSurname()) && !human.equals(person)) {
+                result.add(human);
+            }
+        }
+        return result;
     }
 
-    public List<Human> findPeopleWithSameSurname(List<Human> humanList, Human person){
-        List<Human> result = new ArrayList<>();
-        String surName = person.getSurname();
-        for (Human human: humanList){
-            if (human.getSurname().equals(surName) && !human.equals(person)){
-                result.add(human);
+    //Метод удаления выбранного человечка
+    public List<Human> removeSelectedPerson(List<Human> humanList, Human targetPerson) {
+        List<Human> copyList = new ArrayList<>(humanList);
+        copyList.remove(humanList);
+        return copyList;
+
+    }
+
+    //Метод нахождения стариков
+    public Set<Human> findMaxAgePeople(List<Human> peopleList) {
+        int maxAge = 0;
+        Set<Human> veryOldPeople = new HashSet<>();
+
+        for (Human person : peopleList) {
+            if (person.getAge() > maxAge) {
+                maxAge = person.getAge();
+                veryOldPeople.add(person);
 
             }
         }
+        return veryOldPeople;
+    }
 
+    public Set<Human> findPeopleByIndex(Map<Integer, Human> idHumanList, Set<Integer> ids){
+
+        Set<Human> result = new HashSet<>();
+        for (int id: ids){
+            Human person = idHumanList.get(id);
+            if (person != null){
+                result.add(person);
+            }
+        }
         return result;
     }
 }
+
